00c7050e178182c26da5ee54da4907b929a94b92
Kevin <kev@ktzr.uk>
2017-02-11 16:10:33 +0000

    add a serch bar and auto complete for search bar

---
+++ BetterThanStephen/static/js/autocomplete.js
@@ -0,0 +1,19 @@
+/**
+ * Created by the_k on 11/02/2017.
+ */
+var word_list = [
+    'foobar',
+    'foo',
+    'bar',
+    'baz',
+    'qux',
+    'quux',
+    'quuz',
+    'corge',
+    'grault',
+    'garply',
+    'waldo',
+    'fred',
+    'plugh',
+    'xyzzy',
+    'thud']
