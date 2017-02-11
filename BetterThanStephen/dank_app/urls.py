4f9635f81cc121f7e9620f76440bb8a97012ef5a
Nathan Douglas <nd9600@gmail.com>
2017-02-11 16:38:46 +0000

    added speech view

--- BetterThanStephen/dank_app/urls.py
+++ BetterThanStephen/dank_app/urls.py
@@ -2,5 +2,6 @@ from dank_app import views
 from django.conf.urls import patterns, url, include
 
 urlpatterns = [
-    url(r'$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'speech$', views.speech, name='speech'),
 ]
--- BetterThanStephen/dank_app/views.py
+++ BetterThanStephen/dank_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponseRedirect
 
 def index(request):
     return render(request, 'dank_app/index.html')
+
+def speech(request):
+    return render(request, 'dank_app/speech-demo.html')
