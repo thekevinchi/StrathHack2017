04e1be85ebd1675ea3f0e63d82ecfebf451d660b
Nathan Douglas <nd9600@gmail.com>
2017-02-11 16:38:46 +0000

    added speech view

--- BetterThanStephen/dank_app/urls.py
+++ BetterThanStephen/dank_app/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url, include
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/$', views.search, name='search'),
+    url(r'speech/$', views.speech, name='speech'),
 ]
