07aa59d10ccbba7ed10fb7a8ae20bbbfe22edd78
Kevin <kev@ktzr.uk>
2017-02-11 17:20:04 +0000

    see userprofiles and payments in django admin page

---
+++ BetterThanStephen/dank_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from dank_app.models import UserProfile, Payments
+
+admin.site.register(UserProfile)
+admin.site.register(Payments)
+
+# Register your models here.
