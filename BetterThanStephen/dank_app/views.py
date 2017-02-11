3bd23b1951d5208bb0b19cde83a894042e9b5d7b
Nathan Douglas <nd9600@gmail.com>
2017-02-11 19:21:06 +0000

    started finding the keywords

--- BetterThanStephen/dank_app/views.py
+++ BetterThanStephen/dank_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, HttpResponseRedirect
-
+from query_functions import find_keywords, query_db
 
 def index(request):
+    return render(request, 'dank_app/index.html')
+
+def result(request):
+    result_string = request.GET['result_string']
+    keywords = find_keywords(result_string)
+    
+    print("result_string is: {0}".format(result_string))
+    print("keywords are: {0}".format(keywords))
+
+    query_db(keywords)
+	
     return render(request, 'dank_app/index.html')
 
 def speech(request):
