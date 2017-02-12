833b31447048e07df5ebcaf1f11e07fe9a2c218c
Nathan Douglas <nd9600@gmail.com>
2017-02-12 02:47:02 +0000

    made it work with single word numbers

--- BetterThanStephen/dank_app/query_handling.py
+++ BetterThanStephen/dank_app/query_handling.py
@@ -9,8 +9,8 @@ from django.contrib.auth.models import User
 
 from dank_app.models import UserProfile, Payments
 
-amount_list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "1", "2", "3", "4", "5",
-               "6", "7", "8", "9", "10"]
+amount_list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+amount_list.extend(str(i) for i in xrange(0, 100))
 
 possible_keywords = {"question": ["when", "how"],
                      "time": ["next", "last", "due"],
@@ -18,6 +18,35 @@ possible_keywords = {"question": ["when", "how"],
                      "amount": amount_list
                      }
 
+def text2int(textnum, numwords={}):
+    if not numwords:
+      units = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen",
+      ]
+
+      tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+      scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+      numwords["and"] = (1, 0)
+      for idx, word in enumerate(units):    numwords[word] = (1, idx)
+      for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+      for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+          raise Exception("Illegal word: " + word)
+
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
+
+    return result + current
 
 def find_keywords(result_string):
     keywords = {"question": [], "time": [], "noun": [], "amount": []}
@@ -40,14 +69,23 @@ def find_keywords(result_string):
                 keywords["noun"].append("payment")
             elif word == "loans":
                 keywords["noun"].append("loan")
+            elif word == "owe":
+                keywords["noun"].append("payment")
+                keywords["time"].append("next")
             else:
                 keywords["noun"].append(word)
         elif word in possible_keywords["amount"]:
-            keywords["amount"].append(word)
+            print("here")
+            if (len(word) > 2):
+                keywords["amount"].append(text2int(word))
+            else:
+                keywords["amount"].append(int(word))
 
     #if no amount is given, default to 1 record
     if len(keywords["amount"]) == 0:
         keywords["amount"].append(1)
+
+    print keywords
 
     return keywords
 
