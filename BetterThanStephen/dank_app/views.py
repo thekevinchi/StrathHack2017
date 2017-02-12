b52712d72ee0d9f985727d6d9a1e3b2f4b1f0d55
Kevin <kev@ktzr.uk>
2017-02-12 03:10:19 +0000

    updated styling

--- BetterThanStephen/dank_app/views.py
+++ BetterThanStephen/dank_app/views.py
@@ -34,10 +34,10 @@ def search(request):
             # query = request.GET.get('query', '')
             # keywords = find_keywords(query)
             # response = query_db(keywords)
-            context_dict = {'user': user,}
-                        # "query": query,
-                        # "response": response,
-                        # }
+            context_dict = {'user': user, }
+            # "query": query,
+            # "response": response,
+            # }
         return render(request, 'dank_app/search.html', context_dict)
 
     return HttpResponseRedirect('/')
@@ -73,23 +73,22 @@ def graph(request):
     if request.user.is_active:
         user = request.user
         all_payments = Payments.objects.filter(user=user.userprofile)
-        total_to_be_payed =0
-        payments_recived =0
+        total_to_be_payed = 0
+        payments_recived = 0
         for payment in all_payments:
             if payment.paid:
                 payments_recived += payment.amount
             total_to_be_payed += payment.amount
 
-        percent_paid = int((float(payments_recived)/total_to_be_payed)*100)
+        percent_paid = int((float(payments_recived) / total_to_be_payed) * 100)
         graph_path = pos(percent_paid)
         context_dict = {'user': user,
-                        'percent_paid':percent_paid,
-                        'graph_path':graph_path,}
+                        'percent_paid': percent_paid,
+                        'graph_path': graph_path, }
         print context_dict
-        return render(request, 'dank_app/graph.html',context_dict)
+        return render(request, 'dank_app/graph.html', context_dict)
     else:
         return HttpResponseRedirect('/')
-
 
 
 def faq(request):
@@ -100,19 +99,24 @@ def faq(request):
     context_dict = {'user': user}
     return render(request, 'dank_app/faq.html', context_dict)
 
+
 def pos(x):
     # you need to do "from math import *" because I'm a lazy bastard
     if x >= 75:
-        return "M 0.5 0.5 0.5 0 A 0.5 0.5 0 0 1 %f %f z" % ( (sin((100-x)/100.0 * 2 * pi) + 1)/2, (1 - cos((100-x)/100.0 * 2 * pi))/2 )
+        return "M 0.5 0.5 0.5 0 A 0.5 0.5 0 0 1 %f %f z" % (
+        (sin((100 - x) / 100.0 * 2 * pi) + 1) / 2, (1 - cos((100 - x) / 100.0 * 2 * pi)) / 2)
     elif x >= 50:
         res = "M 0.5 0.5 0.5 0 A 0.5 0.5 0 0 1 1 0.5"
-        res += "M 0.5 0.5 1 0.5 A 0.5 0.5 0 0 1 %f %f" % ( (sin((100-x)/100.0 * 2 * pi) + 1)/2, (1 - cos((100-x)/100.0 * 2 * pi))/2 )
+        res += "M 0.5 0.5 1 0.5 A 0.5 0.5 0 0 1 %f %f" % (
+        (sin((100 - x) / 100.0 * 2 * pi) + 1) / 2, (1 - cos((100 - x) / 100.0 * 2 * pi)) / 2)
     elif x >= 25:
         res = "M 0.5 0.5 0.5 0 A 0.5 0.5 0 0 1 1 0.5 M 0.5 0.5 1 0.5 A 0.5 0.5 0 0 1 0.5 1"
-        res += "M 0.5 0.5 0.5 1 A 0.5 0.5 0 0 1 %f %f" % ( (sin((100-x)/100.0 * 2 * pi) + 1)/2, (1 - cos((100-x)/100.0 * 2 * pi))/2 )
+        res += "M 0.5 0.5 0.5 1 A 0.5 0.5 0 0 1 %f %f" % (
+        (sin((100 - x) / 100.0 * 2 * pi) + 1) / 2, (1 - cos((100 - x) / 100.0 * 2 * pi)) / 2)
     else:
         res = "M 0.5 0.5 0.5 0 A 0.5 0.5 0 0 1 1 0.5 M 0.5 0.5 1 0.5 A 0.5 0.5 0 0 1 0.5 1 M 0.5 0.5 0.5 1 A 0.5 0.5 0 0 1 0 0.5"
-        res += "M 0.5 0.5 0 0.5 A 0.5 0.5 0 0 1 %f %f" % ( (sin((100-x)/100.0 * 2 * pi) + 1)/2, (1 - cos((100-x)/100.0 * 2 * pi))/2 )
+        res += "M 0.5 0.5 0 0.5 A 0.5 0.5 0 0 1 %f %f" % (
+        (sin((100 - x) / 100.0 * 2 * pi) + 1) / 2, (1 - cos((100 - x) / 100.0 * 2 * pi)) / 2)
     res += ' z'
     # this will return a svg drawing that you put into the d attribute
     return res
