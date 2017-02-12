7a83cbae835653b7e9055cc95d9ea628bbb81f76
Nathan Douglas <nd9600@gmail.com>
2017-02-12 02:47:02 +0000

    made it work with single word numbers

--- BetterThanStephen/dank_app/query_functions.py
+++ BetterThanStephen/dank_app/query_functions.py
@@ -4,44 +4,21 @@ def query_db(user_p, query):
 
     keywords = find_keywords(query)
 
-    if ((len(keywords["question"]) == 0) or (len(keywords["time"]) == 0) or (len(keywords["noun"]) == 0) or (len(keywords["amount"]) == 0)):
+    if ((len(keywords["question"]) == 0) or (len(keywords["time"]) == 0) or (len(keywords["noun"]) == 0)):
         return []
 
     question = keywords["question"][0]
     time = keywords["time"][0]
     noun = keywords["noun"][0]
-    amountKeyword = keywords["amount"][0]
+    amount = keywords["amount"][0] if len(keywords["amount"]) > 0 else 1
 
     whenBoolean = False
     paidBoolean = False
-    amount = 1
 
     if question == "when":
         whenBoolean = True
 
     if time == "last":
         paidBoolean = True
-
-    if amountKeyword in amount_list:
-        if amountKeyword in {"one", "1"}:
-            amount = 1
-        elif amountKeyword in {"two", "2"}:
-            amount = 2
-        elif amountKeyword in {"three", "3"}:
-            amount = 3
-        elif amountKeyword in {"four", "4"}:
-            amount = 4
-        elif amountKeyword in {"five", "5"}:
-            amount = 5
-        elif amountKeyword in {"six", "6"}:
-            amount = 6
-        elif amountKeyword in {"seven", "7"}:
-            amount = 7
-        elif amountKeyword in {"eight", "8"}:
-            amount = 8
-        elif amountKeyword in {"nine", "9"}:
-            amount = 9
-        elif amountKeyword in {"ten", "10"}:
-            amount = 10
 
     return loan(user_p, paidBoolean, amount)
